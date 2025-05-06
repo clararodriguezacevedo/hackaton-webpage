cee78a15c3d3cbdcf9532d54004843c1133eb040
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2025-05-06 19:04:52 -0300

    small changes to comform to new style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,5 @@
-'use client';
-
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import Layout from '../components/layouts/Main';
-import Fonts from '../components/Fonts';
-import theme from '../lib/theme';
+import { ChakraUIProvider } from './chakra-provider';
 
 export default function RootLayout({
   children,
@@ -14,12 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ChakraProvider theme={theme}>
-          <Fonts />
-          <Layout>
-            {children}
-          </Layout>
-        </ChakraProvider>
+        <ChakraUIProvider>{children}</ChakraUIProvider>
       </body>
     </html>
   );
